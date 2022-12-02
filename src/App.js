de86678c7d634638a28220c9ac49fa2afcf892a1
inyeong-kang <tomatorkd@naver.com>
2022-12-02 15:02:50 +0900

    feat: logout

--- src/App.js
+++ src/App.js
@@ -9,23 +9,47 @@ import { Helmet } from "react-helmet";
 import FirstLanding from "./pages/FirstLanding.jsx";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<FirstLanding />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/parking" element={<Parking/>} />
-        <Route path="/parking-get" element={<ParkingGet/>} />
-        
-      </Routes>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
-      </Helmet>
-    </>
-  );
+    return ( <
+        >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < FirstLanding / > }
+        /> <
+        Route path = "/login"
+        element = { < Login / > }
+        /> <
+        Route path = "/register"
+        element = { < RegisterPage / > }
+        /> <
+        Route path = "/parking"
+        element = { < Parking / > }
+        /> <
+        Route path = "/parking-get"
+        element = { < ParkingGet / > }
+        /> <
+        Route path = "/logout"
+        element = { < ParkingGet / > }
+        />
+
+        <
+        /Routes> <
+        Helmet >
+        <
+        link rel = "preconnect"
+        href = "https://fonts.googleapis.com" / >
+        <
+        link rel = "preconnect"
+        href = "https://fonts.gstatic.com"
+        crossorigin / >
+        <
+        link href = "https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap"
+        rel = "stylesheet" / >
+        <
+        /Helmet> <
+        />
+    );
 };
 
 export default App;
